194857be118e615232244c9ce2657ad49e7d23d8
cuongngo1121 <cuongdola01012004@gmail.com>
2025-09-08 18:49:25 +0700

    fix: update BASE_URL to Render API

--- src/store/thunk.js
+++ src/store/thunk.js
@@ -1,7 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL =
+    import.meta.env.MODE === 'development'
+        ? 'http://localhost:5000/api'
+        : 'https://api-zing-p20v.onrender.com/api';
+
 
 export const getNewReleaseChart = createAsyncThunk(
     'song/getNewReleaseChart',
